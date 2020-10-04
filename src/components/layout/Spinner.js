c807ab2d154b23eb2f353920ff8aec1d78bcd540
akforu <ashwinak1211@gmail.com>
2020-10-04 23:49:38 +0530

    The 'loading' word in search action was misleading, so i changed it to 'Sorry! check your keywords' for more accurate context.It is your wish to merge!

--- src/components/layout/Spinner.js
+++ src/components/layout/Spinner.js
@@ -4,7 +4,7 @@ export default function Spinner() {
     return (
         <div>
             <img src={Loader} style={{width:'300px',margin:'40px auto',display:'block'}}alt="no"/>
-            <h1 className="display-2">Loading</h1>
+            <h1 className="display-2">Sorry! check your keywords</h1>
         </div>
     )
 }
